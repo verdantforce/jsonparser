326982d822537aa00c782afdd36d7abf72bcb743
Di Zhuang <dzhuang2@gmail.com>
2023-02-24 22:42:30 -0600

    adding a bunch of random stuff.  At least the tests seem to pass

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
 #![allow(unused_imports)]
 #![allow(dead_code)]
-use std::{collections::HashMap, ops, vec};
+use std::{collections::HashMap, ops, string, vec};
+// use std::ops::{Shl, Shr};
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum JsonValue {
     JsonNull,
     JsonBool(bool),
-    JsonNumber(i64), // TODO(make this parse floats etc later)
+    JsonNumber(u64), // TODO(make this parse floats etc later)
     JsonString(String),
     JsonArray(Vec<Box<JsonValue>>),
     JsonObject(HashMap<String, Box<JsonValue>>),
@@ -33,6 +34,7 @@ where
 }
 
 fn unit_p<T: Clone>(value: T) -> impl Parser<T> {
+    // leave the string unparsed and inject value
     move |input: String| {
         Some(ParseResult {
             value: value.clone(),
@@ -118,25 +120,120 @@ fn or<A>(pa1: impl Parser<A>, pa2: impl Parser<A>) -> impl Parser<A> {
     move |input: String| pa1.parse(input.clone()).or(pa2.parse(input))
 }
 
-fn many<A: Clone>(pa: impl Parser<A>) -> impl Parser<Vec<A>>
-{
-    move |input: String| match pa.parse(input.clone()) {
-        Some(ParseResult { value, s }) => map2(unit_p(value), many(pa), |value, vs| {
-            let mut v = vec![value];
-            v.extend(vs);
-            v
+// fn many<A: Clone>(pa: impl Parser<A>) -> impl Parser<Vec<A>>
+// {
+//     move |input: String| match pa.parse(input.clone()) {
+//         Some(ParseResult { value, s }) => map2(unit_p(value), many(pa), |value, vs| {
+//             let mut v = vec![value];
+//             v.extend(vs);
+//             v
+//         })
+//         .parse(s),
+//         None => Some(ParseResult {
+//             value: Vec::new(),
+//             s: input,
+//         }),
+//     }
+// }
+
+fn many1<A: Clone>(pa: impl Parser<A>) -> impl Parser<Vec<A>> {
+    move |input: String| {
+        let mut v = Vec::new();
+        let mut next_input = input.clone();
+
+        while let Some(ParseResult { value, s }) = pa.parse(next_input.clone()) {
+            next_input = s;
+            v.push(value);
+        }
+        Some(ParseResult {
+            value: v,
+            s: next_input,
         })
-        .parse(s),
-        None => Some(ParseResult {
-            value: Vec::new(),
-            s: input,
-        }),
     }
+}
+
+fn left<A>(pa: impl Parser<A>, pb: impl Parser<A>) -> impl Parser<A> {
+    // run both parsers but keep the result from the left parser
+    map2(pa, pb, |a, _b| a)
+}
+
+fn right<A>(pa: impl Parser<A>, pb: impl Parser<A>) -> impl Parser<A> {
+    // run both parsers but keep the result from the right parser
+    map2(pa, pb, |_a, b| b)
+}
+
+fn parse_null() -> impl Parser<JsonValue> {
+    fmap(string_p("null".to_owned()), |_s: String| {
+        JsonValue::JsonNull
+    })
+}
+
+fn parse_bool() -> impl Parser<JsonValue> {
+    fmap(
+        or(string_p("true".to_owned()), string_p("false".to_owned())),
+        |s: String| match &s[..] {
+            "true" => JsonValue::JsonBool(true),
+            _ => JsonValue::JsonBool(false),
+        },
+    )
+}
+
+fn parse_quote() -> impl Parser<String> {
+    string_p("\"".to_string())
+}
+
+fn parse_string() -> impl Parser<JsonValue> {
+    // TODO: handle escaped quotes
+    // TODO: make it so that this can be written as parse_quote() >> while_p(|&c| c != '"') << parse_quote()
+    fmap(
+        left(right(parse_quote(), while_p(|&c| c != '"')), parse_quote()),
+        |s| JsonValue::JsonString(s),
+    )
+}
+
+fn parse_number() -> impl Parser<JsonValue> {
+    // TODO: make this better
+    // TODO: should there be and_then on Parser itself?
+    move |input: String| {
+        while_p(|&c| c.is_digit(10))
+            .parse(input)
+            .and_then(|ParseResult { value, s }| {
+                if !value.is_empty() {
+                    Some(ParseResult {
+                        value: JsonValue::JsonNumber(value.parse().unwrap()),
+                        s: s,
+                    })
+                } else {
+                    None
+                }
+            })
+    }
+}
+
+fn parse_json1() -> impl Parser<JsonValue> {
+    // TODO: make it so that it is parse_null() | parse_bool() | parse_number() ...
+    or(
+        or(or(parse_null(), parse_bool()), parse_number()),
+        parse_string(),
+    )
+}
+
+pub fn parse_json(input: String) -> Option<JsonValue> {
+    parse_json1().parse(input).and_then(
+        |ParseResult { value, s }| {
+            if s.is_empty() {
+                Some(value)
+            } else {
+                None
+            }
+        },
+    )
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use JsonValue::*;
 
     #[test]
     fn test_string_p_success() {
@@ -243,12 +340,24 @@ mod tests {
         );
     }
 
+    // #[test]
+    // fn test_many() {
+    //     let pa = string_p("a".to_owned());
+    //     let pb = many(pa);
+    //     let expected_value:Vec<String> = vec!["a".to_string(); 5];
+    //     if let Some(ParseResult{value, s}) = pb.parse("aaaaabbbb".to_owned()) {
+    //         assert_eq!(value.join(""), "aaaaa");
+    //         assert_eq!(s, "bbbb".to_owned());
+    //     } else {
+    //         panic!("parse failed!");
+    //     }
+    // }
+
     #[test]
-    fn test_many() {
+    fn test_many1() {
         let pa = string_p("a".to_owned());
-        let pb = many(pa);
-        let expected_value:Vec<String> = vec!["a".to_string(); 5];
-        if let Some(ParseResult{value, s}) = pb.parse("aaaaabbbb".to_owned()) {
+        let pb = many1(pa);
+        if let Some(ParseResult { value, s }) = pb.parse("aaaaabbbb".to_owned()) {
             assert_eq!(value.join(""), "aaaaa");
             assert_eq!(s, "bbbb".to_owned());
         } else {
@@ -256,4 +365,72 @@ mod tests {
         }
     }
 
+    #[test]
+    fn test_parse_null() {
+        assert_eq!(
+            parse_null().parse("null_hello".to_string()),
+            Some(ParseResult {
+                value: JsonNull,
+                s: "_hello".to_owned(),
+            })
+        );
+        assert_eq!(parse_null().parse("hello".to_string()), None,);
+    }
+
+    #[test]
+    fn test_parse_bool() {
+        assert_eq!(
+            parse_bool().parse("true_hello".to_string()),
+            Some(ParseResult {
+                value: JsonBool(true),
+                s: "_hello".to_owned(),
+            })
+        );
+        assert_eq!(
+            parse_bool().parse("false_hello".to_string()),
+            Some(ParseResult {
+                value: JsonBool(false),
+                s: "_hello".to_owned(),
+            })
+        );
+        assert_eq!(parse_bool().parse("foo_hello".to_string()), None,);
+    }
+
+    #[test]
+    fn test_parse_string() {
+        assert_eq!(
+            parse_string().parse("\"hello\"friend".to_string()),
+            Some(ParseResult {
+                value: JsonString("hello".to_string()),
+                s: "friend".to_owned(),
+            })
+        );
+        assert_eq!(
+            // fails due to lack of end quote
+            parse_bool().parse("\"hello".to_string()),
+            None,
+        );
+    }
+
+    #[test]
+    fn test_parse_number() {
+        assert_eq!(
+            parse_number().parse("1234hello".to_string()),
+            Some(ParseResult {
+                value: JsonNumber(1234),
+                s: "hello".to_owned(),
+            })
+        );
+    }
+
+    #[test]
+    fn test_parse_json() {
+        assert_eq!(parse_json("null".to_string()), Some(JsonNull),);
+        assert_eq!(parse_json("true".to_string()), Some(JsonBool(true)),);
+        assert_eq!(parse_json("1234".to_string()), Some(JsonNumber(1234)),);
+        assert_eq!(
+            parse_json("\"foo\"".to_string()),
+            Some(JsonString("foo".to_string())),
+        );
+    }
 }
